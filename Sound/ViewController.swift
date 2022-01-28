772add1250bf15933671cbe2c414f2c3f9c812b6
SeinaKonishi <fzf55747@kwansei.ac.jp>
2022-01-29 00:11:50 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by SeinaKonishi on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
